ec6b7c97c041a76ef886e510d68b0bcc9871ae1e
dkmrn <168375784+dkmrn@users.noreply.github.com>
2024-04-28 13:56:40 -0700

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_AI_KEY"))
+
+
+def chatbot():
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+
+    numIngredients = 3
+    while True:
+        ingredients = []
+        for i in range(numIngredients):
+            message = input(f"Ingredient {i+1}: ")
+            if message.lower() == "quit":
+                return 0;     
+            ingredients.append(message)
+        recipePrompt = "Give me a recipe using the following ingredients: "
+        for i in range(numIngredients):
+            recipePrompt = recipePrompt + ingredients[i] + ", "
+            
+        messages.append({"role": "user", "content": recipePrompt})
+
+        response = client.chat.completions.create(model = "gpt-3.5-turbo",
+        messages=messages)
+
+        chat_message = response.choices[0].message.content
+        print(f"Bot: {chat_message}")
+        messages.append({"role": "assistant", "content": chat_message})
+if __name__ == "__main__":
+    print("Start chatting with the bot (type 'quit' to stop)!")
+    chatbot()
