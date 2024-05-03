998e91f0c1464574a7140475054f01fb1e752aad
dkmrn <168375784+dkmrn@users.noreply.github.com>
2024-05-03 13:58:26 -0700

    Update chat.py
    It works sometimes but not all of the time

--- chat.py
+++ chat.py
@@ -1,33 +1,62 @@
 import os
 from openai import OpenAI
+from os import system
+import speech_recognition as sr
 
 client = OpenAI(api_key=os.getenv("OPENAI_AI_KEY"))
+r = sr.Recognizer()
+
+def getSpeech():
+    while True:
+
+        try:
+            
+            # use the microphone as source for input.
+            with sr.Microphone() as source2:
+
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                
+                #listens for the user's input 
+                print("Listening...")
+                audio2 = r.listen(source2)
+                
+                # Using google to recognize audio
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
+
+                #Print text to screen and speak text
+                print("User: ", MyText)
+                return MyText
+                
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("unknown error occurred")
+
+def speak(text):
+
+    #output text as sound "spoken" by computer
+    system("say {}".format(text))
 
 def chatbot():
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-    ]
-
-    numIngredients = 3
+    messages = [{"role": "system", "content": "You are a helpful assistant."},]
+    
     while True:
-        ingredients = []
-        for i in range(numIngredients):
-            message = input(f"Ingredient {i+1}: ")
-            if message.lower() == "quit":
-                return 0;     
-            ingredients.append(message)
-        recipePrompt = "Give me a recipe using the following ingredients: "
-        for i in range(numIngredients):
-            recipePrompt = recipePrompt + ingredients[i] + ", "
-            
-        messages.append({"role": "user", "content": recipePrompt})
+
+        message = getSpeech()
+        if message.lower() == "quit":
+            break
+                
+        messages.append({"role": "user", "content": message})
 
         response = client.chat.completions.create(model = "gpt-3.5-turbo", messages=messages)
 
         chat_message = response.choices[0].message.content
         print(f"Bot: {chat_message}")
+        speak(chat_message)
         messages.append({"role": "assistant", "content": chat_message})
-        
+            
 if __name__ == "__main__":
-    print("Enter three ingredients and I'll suggest a recipe!(type 'quit' to stop)!")
+    print("Start chatting with the bot (say 'quit' to stop)! ")
     chatbot()
